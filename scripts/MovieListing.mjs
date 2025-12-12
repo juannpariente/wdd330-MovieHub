7b2a9f48c61e8915499012aca7123b138961c5f6
juannpariente <jpariente000@gmail.com>
2025-12-12 04:02:01 -0300

    I add movie favorite function

--- scripts/MovieListing.mjs
+++ scripts/MovieListing.mjs
@@ -1,9 +1,38 @@
+import { getLocalStorage, setLocalStorage } from "./Tools.mjs";
+
 export function movieCardTemplate(movieData) {
     return `<li class="movie-card">
         <a href="/wdd330-MovieHub/movie-details.html?movie=${movieData.id}">
             <img src="https://image.tmdb.org/t/p/w500${movieData.poster_path}" alt="${movieData.original_title}">
             <h2>${movieData.title}</h2>
         </a>
+        <button class="fav-btn" data-id="${movieData.id}">★</button>
     </li>`
+}
+
+export function setupFavoriteButtons() {
+    const buttons = document.querySelectorAll(".fav-btn");
+    let favs = getLocalStorage("fav");
+
+    buttons.forEach(btn => {
+        const movieId = Number(btn.dataset.id);
+
+        if (favs.includes(movieId)) {
+            btn.classList.add("active");
+        }
+
+        btn.addEventListener("click", () => {
+
+            if (favs.includes(movieId)) {
+                favs = favs.filter(id => id !== movieId);
+                btn.classList.remove("active");
+            } else {
+                favs.push(movieId);
+                btn.classList.add("active");
+            }
+
+            setLocalStorage("fav", favs);
+        });
+    });
 }
 
--- scripts/movies.js
+++ scripts/movies.js
@@ -1,5 +1,5 @@
 import { initHeaderFooter } from "./HeaderFooter.mjs";
-import { movieCardTemplate } from "./MovieListing.mjs";
+import { movieCardTemplate, setupFavoriteButtons } from "./MovieListing.mjs";
 import { renderListWithTemplate } from "./Tools.mjs";
 import { getMovieData } from "./ExternalData.mjs";
 
@@ -10,6 +10,8 @@ const movieList = document.querySelector("#movie-list");
 async function displayMovies() {
     const movies = await getMovieData("movie/popular?language=en-US&page=1");
     renderListWithTemplate(movieCardTemplate, movieList, movies.results);
+
+    setupFavoriteButtons()
 }
 
 displayMovies();
