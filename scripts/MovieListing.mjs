378d6a5466f3924b298013539f327cec8fbcd582
juannpariente <jpariente000@gmail.com>
2025-12-12 01:08:42 -0300

    fix an url for github pages

--- scripts/MovieListing.mjs
+++ scripts/MovieListing.mjs
@@ -1,6 +1,6 @@
 export function movieCardTemplate(movieData) {
     return `<li class="movie-card">
-        <a href="movie-details.html?movie=${movieData.id}">
+        <a href="/wdd330-MovieHub/movie-details.html?movie=${movieData.id}">
             <img src="https://image.tmdb.org/t/p/w500${movieData.poster_path}" alt="${movieData.original_title}">
             <h2>${movieData.title}</h2>
         </a>
